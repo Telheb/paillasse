e702e2f9bc3cf7be05b5cb7e498f11dc520296ad
emmanuel <garciaemmanuel@hotmail.com>
2020-08-11 12:16:52 +0200

    debut du projet

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="room">
+
+<!-- block elements -->
+<div class="element" id="e1"></div>
+<div class="element" id="e2"></div>
+<div class="element" id="e3"></div>
+<div class="element" id="e4"></div>
+<div class="element" id="e5"></div>
+<div class="element" id="e6"></div>
+
+<!-- block ouTil -->
+<div class="outil" id="o1"></div>
+<div class="outil" id="o2"></div>
+<div class="outil" id="o3"></div>
+<div class="outil" id="o4"></div>
+<div class="outil" id="o5"></div>
+<div class="outil" id="o6"></div>
+
+<!-- armoire des éléments -->
+<div class="armoire_1" id="a1"></div>
+
+<!-- hote -->
+<div class="hote"></div>
+
+<!-- armoire des tools -->
+<div class="armoire_2" id="a2"></div>
+
+<!-- empty espaces -->
+<div class="emptyspace" id="es1"></div>
+<div class="emptyspace" id="es2"></div>
+<div class="emptyspace" id="es3"></div>
+
+<!-- table de travail -->
+<div class="paillasse"></div>
+
+<!-- armoire de la paillasse gauche -->
+<div class="armoire_3" id="a3"></div>
+
+<!-- armoire de la paillasse droite -->
+<div class="armoire_4" id="a4"></div>
+
+<!-- trash -->
+<div class="trash"></div>
+
+
+    </div>
+    
+</body>
+</html>
