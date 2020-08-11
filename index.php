269740458cadbf1e0fbaa0745debfb8085539a2c
emmanuel <garciaemmanuel@hotmail.com>
2020-08-11 15:38:46 +0200

    préparation travail

--- index.php
+++ index.php
@@ -3,54 +3,58 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="public/css/style.css">
     <title>Document</title>
 </head>
 <body>
     <div class="room">
 
-<!-- block elements -->
-<div class="element" id="e1"></div>
-<div class="element" id="e2"></div>
-<div class="element" id="e3"></div>
-<div class="element" id="e4"></div>
-<div class="element" id="e5"></div>
-<div class="element" id="e6"></div>
+        <!-- block elements -->
+        <div class="element" id="e1"></div>
+        <div class="element" id="e2"></div>
+        <div class="element" id="e3"></div>
+        <div class="element" id="e4"></div>
+        <div class="element" id="e5"></div>
 
-<!-- block ouTil -->
-<div class="outil" id="o1"></div>
-<div class="outil" id="o2"></div>
-<div class="outil" id="o3"></div>
-<div class="outil" id="o4"></div>
-<div class="outil" id="o5"></div>
-<div class="outil" id="o6"></div>
 
-<!-- armoire des éléments -->
-<div class="armoire_1" id="a1"></div>
+        <!-- block ouTil -->
+        <div class="outil" id="o1"></div>
+        <div class="outil" id="o2"></div>
+        <div class="outil" id="o3"></div>
+        <div class="outil" id="o4"></div>
+        <div class="outil" id="o5"></div>
 
-<!-- hote -->
-<div class="hote"></div>
 
-<!-- armoire des tools -->
-<div class="armoire_2" id="a2"></div>
+        <!-- armoire des éléments -->
+        <div class="armoire_1" id="a1"></div>
 
-<!-- empty espaces -->
-<div class="emptyspace" id="es1"></div>
-<div class="emptyspace" id="es2"></div>
-<div class="emptyspace" id="es3"></div>
+        <!-- hote -->
+        <div class="hote"></div>
 
-<!-- table de travail -->
-<div class="paillasse"></div>
+        <!-- armoire des tools -->
+        <div class="armoire_2" id="a2"></div>
 
-<!-- armoire de la paillasse gauche -->
-<div class="armoire_3" id="a3"></div>
+        <!-- empty espaces -->
+        <div class="emptyspace" id="es1"></div>
+        <div class="emptyspace" id="es2"></div>
+        <div class="emptyspace" id="es3"></div>
+        <div class="emptyspace" id="es4"></div>
+        <div class="emptyspace" id="es5"></div>
 
-<!-- armoire de la paillasse droite -->
-<div class="armoire_4" id="a4"></div>
 
-<!-- trash -->
-<div class="trash"></div>
+        <!-- table de travail -->
+        <div class="paillasse"></div>
 
+        <!-- armoire de la paillasse gauche -->
+        <div class="armoire_3" id="a3"></div>
+
+        <!-- armoire de la paillasse droite -->
+        <div class="armoire_4" id="a4"></div>
+
+        <!-- trash -->
+        <div class="trash" id="t1"></div>
+        <div class="trash" id="t2"></div>
+        <div class="trash" id="t3"></div>
 
     </div>
     
